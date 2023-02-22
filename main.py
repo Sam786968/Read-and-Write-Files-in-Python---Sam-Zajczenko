2956605269e1718cf6c33e2feaba2f05319ab6f7
Sam Zajczenko <113551869+Sam786968@users.noreply.github.com>
2023-02-22 20:11:49 +0000

    working with packages

--- main.py
+++ main.py
@@ -41,3 +41,23 @@
 # with open('user.txt', 'a') as f:
 #   input = ("How was your day? ")
 #   f.write(input)
+
+# --- Second Group of Work ---
+# import urllib
+# import json
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.amazon.com/Best-Sellers-Books/zgbs/books"
+headers = {
+  'user_agent':
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+}
+
+page = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
+
+books = soup.find_all()
